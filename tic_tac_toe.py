c9c50e2ad8d82023fb4cdd5e1e5222e0581ee2f6
David Campbell <tryggus20@gmail.com>
2024-05-09 10:50:50 -0500

    board setup

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,5 @@
 # tic-tac-toe game using Tkinter. Future use will include AI 
-# tkinter must be at least 8.6
+# tkinter must be at least Version 8.6
 import tkinter 
 tkinter.TkVersion
 from itertools import cycle
@@ -20,3 +20,22 @@ DEFAULT_PLAYERS = (
         Player(label="X", color="red"),
         Player(label="O", color="blue"),
 )
+
+class TicTacToeGame:
+        def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+                self._players = cycle(players)
+                self.board_size = board_size
+                self.current_player = next(self._players)
+                self.winner_combo = []
+                self._current_moves = []
+                self._has_winner = False
+                self._winning_combos = []
+                self._setup_board()
+       
+        def _setup_board(self):
+                self._current_moves = [
+                        [Move(row, col) for col in range(self.board_size)]
+                        for row in range(self.board_size)
+                ]
+                self._winning_combos = self._get_winning_combos()
+                
